8e41502f2efae4e16c2075896eacfdeeb214d842
maryamkhoolia <maryamkhoolia22@gmail.com>
2023-07-09 14:34:15 +0330

    Update index.html

--- src/app.js
+++ src/app.js
@@ -28,12 +28,14 @@ function displayTemperature(response) {
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElemant.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  iconElement.innerHTML = `https://openweathermap.org/img/wn/10d@2x.png`;
 }
 let apiKey = "ce1cf622991644d8b6693d690ebcfb42";
 let city = "Shiraz";
